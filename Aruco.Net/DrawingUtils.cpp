8cf695e31d0ec0110bf2dfe0ceae64d2f8e5626e
glopes <glopesdev@users.noreply.github.com>
2014-03-04 02:00:42 +0000

    Renamed CvDrawingUtils class to DrawingUtils for consistency.

--- Aruco.Net/DrawingUtils.cpp
+++ Aruco.Net/DrawingUtils.cpp
@@ -1,18 +1,18 @@
 #include "StdAfx.h"
-#include "CvDrawingUtils.h"
+#include "DrawingUtils.h"
 
 
-Aruco::Net::CvDrawingUtils::CvDrawingUtils()
+Aruco::Net::DrawingUtils::DrawingUtils()
 {
 }
 
-void Aruco::Net::CvDrawingUtils::Draw3dAxis(OpenCV::Net::Arr ^image, Marker ^marker, CameraParameters ^parameters)
+void Aruco::Net::DrawingUtils::Draw3dAxis(OpenCV::Net::Arr ^image, Marker ^marker, CameraParameters ^parameters)
 {
 	cv::Mat cvimage = cv::cvarrToMat(image->DangerousGetHandle().ToPointer());
 	aruco::CvDrawingUtils::draw3dAxis(cvimage, *(marker->marker), *(parameters->parameters));
 }
 
-void Aruco::Net::CvDrawingUtils::Draw3dCube(OpenCV::Net::Arr ^image, Marker ^marker, CameraParameters ^parameters)
+void Aruco::Net::DrawingUtils::Draw3dCube(OpenCV::Net::Arr ^image, Marker ^marker, CameraParameters ^parameters)
 {
 	cv::Mat cvimage = cv::cvarrToMat(image->DangerousGetHandle().ToPointer());
 	aruco::CvDrawingUtils::draw3dCube(cvimage, *(marker->marker), *(parameters->parameters));
