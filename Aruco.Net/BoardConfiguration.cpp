d615241f7ea444274b13e3e8f68fc512e218a4b5
glopes <glopes@localhost>
2014-03-12 13:54:20 +0000

    Added support for saving and reading marker board configuration from a file.

--- Aruco.Net/BoardConfiguration.cpp
+++ Aruco.Net/BoardConfiguration.cpp
@@ -3,3 +3,35 @@
 
 using namespace System::Runtime::InteropServices;
 
+void Aruco::Net::BoardConfiguration::SaveToFile(String ^fileName)
+{
+	IntPtr hFileName = Marshal::StringToHGlobalAnsi(fileName);
+	std::string filePath((char *)hFileName.ToPointer());
+
+	aruco::BoardConfiguration bConfig;
+	bConfig.mInfoType = (int)markerInfoType;
+	for each (MarkerInfo ^markerInfo in markers)
+	{
+		bConfig.push_back(*markerInfo->markerInfo);
+	}
+
+	bConfig.saveToFile(filePath);
+	Marshal::FreeHGlobal(hFileName);
+}
+
+void Aruco::Net::BoardConfiguration::ReadFromFile(String ^fileName)
+{
+	IntPtr hFileName = Marshal::StringToHGlobalAnsi(fileName);
+	std::string filePath((char *)hFileName.ToPointer());
+
+	aruco::BoardConfiguration bConfig;
+	bConfig.readFromFile(filePath);
+	Marshal::FreeHGlobal(hFileName);
+
+	markerInfoType = (Aruco::Net::MarkerInfoType)bConfig.mInfoType;
+	std::vector<aruco::MarkerInfo>::const_iterator it;
+	for (it = bConfig.begin(); it != bConfig.end(); it++)
+	{
+		markers->Add(gcnew MarkerInfo(*it));
+	}
+}
