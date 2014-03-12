31b1e4b95ac5cac708463f935005200ba1c74b49
glopes <glopes@localhost>
2014-03-12 11:47:50 +0000

    Added support for marker board drawing methods.

--- Aruco.Net/DrawingUtils.cpp
+++ Aruco.Net/DrawingUtils.cpp
@@ -12,8 +12,20 @@ void Aruco::Net::DrawingUtils::Draw3dAxis(OpenCV::Net::Arr ^image, Marker ^marke
 	aruco::CvDrawingUtils::draw3dAxis(cvimage, *(marker->marker), *(parameters->parameters));
 }
 
+void Aruco::Net::DrawingUtils::Draw3dAxis(OpenCV::Net::Arr ^image, Board ^board, CameraParameters ^parameters)
+{
+	cv::Mat cvimage = cv::cvarrToMat(image->DangerousGetHandle().ToPointer());
+	aruco::CvDrawingUtils::draw3dAxis(cvimage, *(board->board), *(parameters->parameters));
+}
+
 void Aruco::Net::DrawingUtils::Draw3dCube(OpenCV::Net::Arr ^image, Marker ^marker, CameraParameters ^parameters)
 {
 	cv::Mat cvimage = cv::cvarrToMat(image->DangerousGetHandle().ToPointer());
 	aruco::CvDrawingUtils::draw3dCube(cvimage, *(marker->marker), *(parameters->parameters));
+}
+
+void Aruco::Net::DrawingUtils::Draw3dCube(OpenCV::Net::Arr ^image, Board ^board, CameraParameters ^parameters)
+{
+	cv::Mat cvimage = cv::cvarrToMat(image->DangerousGetHandle().ToPointer());
+	aruco::CvDrawingUtils::draw3dCube(cvimage, *(board->board), *(parameters->parameters));
 }
